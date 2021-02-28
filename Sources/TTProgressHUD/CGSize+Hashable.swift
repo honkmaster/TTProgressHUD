9366657c570a2d62cd9fdc5f72a311264e750024
Tobias Tiemerding <tobias@tiemerding.com>
2021-02-28 12:45:32 +0100

    Add CGSize+Hashable.swift

---
+++ Sources/TTProgressHUD/CGSize+Hashable.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by Tobias Tiemerding on 27.02.21.
+//
+
+import CoreGraphics
+
+extension CGSize: Hashable {
+     public func hash(into hasher: inout Hasher) {
+         hasher.combine(width)
+         hasher.combine(height)
+     }
+ }
