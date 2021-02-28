39609bac030d1797a48cede8821285fedf3e955c
David Hart <david@hartbit.com>
2021-02-28 10:09:11 +0100

    Fix Hashable conformance

--- Sources/TTProgressHUD/TTProgressHUDConfig.swift
+++ Sources/TTProgressHUD/TTProgressHUDConfig.swift
@@ -103,3 +103,10 @@ public struct TTProgressHUDConfig: Hashable {
         self.hapticsEnabled = hapticsEnabled
     }
 }
+
+extension CGSize: Hashable {
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(width)
+        hasher.combine(height)
+    }
+}
